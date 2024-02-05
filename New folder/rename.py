a80b4e548f3245422eddf058513a05101e687966
jd-co1 <156161910+jd-co1@users.noreply.github.com>
2024-02-05 16:26:59 +0530

    Delete New folder directory

--- New folder/rename.py
+++
@@ -1,9 +0,0 @@
-import os,glob
-
-list_of_files = glob.glob(os.path.join("C:\\Users\\sheik jaheer\\Downloads\\", '*'))
-latest_file = max(list_of_files, key=os.path.getctime)
-latest_file_path = os.path.join("C:\\Users\\sheik jaheer\\Downloads\\", latest_file)
-new_file_path = os.path.join("C:\\Users\\sheik jaheer\\Downloads\\", 'options.csv')
-
-# Rename the file
-os.rename(latest_file_path, new_file_path)
