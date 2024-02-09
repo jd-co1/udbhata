0acf5a80ae501fc680a6affbda01ba215f7bb6d0
jd-co1 <sheik.javeed@tynybay.com>
2024-02-09 16:12:54 +0530

    k

--- views/companies.py
+++ views/companies.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from TSR_calc import tsr,history
-from TSR_we_bscraping import historical_share_csv_web_scraping
 from average_tenure import average_tenure_of_a_company
 from financial import Ebitda,debt_to_equity,current_investment,ROCE_2023,ESG,Awards,Board_industry_skills
 # from country import stre
