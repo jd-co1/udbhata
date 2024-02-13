e996b7f11bcd41943827bc3eaa4ca7491dbdb068
jd-co1 <sheik.javeed@tynybay.com>
2024-02-13 12:34:12 +0530

    updateed for novartis

--- views/companies.py
+++ views/companies.py
@@ -22,7 +22,7 @@ def load_companies():
         placeholder="companies",
         
         )
-    if option == "drreddy" or option == "abbott" or option == "gsk":
+    if option == "drreddy" or option == "abbott" or option == "gsk" or option == "novartis":
         st.write('You selected:', option)
         if option == f'{option}':
             # st.session_state.company_selected = option
