c0f654a8222797b96a6e42473664b5fb641f3e3c
jd-co1 <sheik.javeed@tynybay.com>
2024-02-19 11:07:22 +0530

    Updated cumulative  Finanial score

---
+++ RIsk_score/Financial_score.py
@@ -0,0 +1,96 @@
+from pymongo import MongoClient
+import os,math,re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=MongoClient('mongodb+srv://test:test@cluster0.tw5ieeh.mongodb.net/?retryWrites=true&w=majority')
+mydb=client.get_database('Udbhata')
+information=mydb.companies_data
+
+def ebit_netsales(option):
+    k=information.find_one({"name":f"{option}"})
+    if 'ebit/net_sales' in k:
+        average_days=k['ebit/net_sales']
+        if average_days=="NA":
+            return 0
+        else:
+            if not isinstance(average_days, str):
+                average_days=str(average_days)
+            average_days=average_days.replace("%","")
+            average_days=float(average_days)
+            return round(average_days,2)
+            # return round(average_days,2) #average_tenure
+    else:
+        return"average_tenure not found in any document"
+def EBIT_Net_Sales(option):
+        drreddy=ebit_netsales('drreddy')
+        novartis=ebit_netsales('novartis')
+        abbott=ebit_netsales('abbott')
+        gsk=ebit_netsales('gsk')
+        teva=ebit_netsales('teva')
+        takeda=ebit_netsales('takeda')
+        k={'drreddy': drreddy, 'novartis': novartis, 'abbott': abbott, 'gsk': gsk, 'teva': teva, 'takeda': takeda}
+        print(k)
+        list1=sorted(k.values())
+        print(list1)
+        op=k.get(option)
+        print(op)
+        if op==0:
+            gsk_position=0
+        else:
+            gsk_position=list1.index(op)
+        print(gsk_position)
+        percent=(gsk_position/len(list1))*100
+        print(percent)
+        if percent==0:
+            gsk_percent1=0
+        else:
+            gsk_percent1=(100-percent)*0.03
+        return gsk_percent1
+        # print(gsk_percent1)
+
+# print(EBIT_Net_Sales('teva'))
+def debt(option):
+    k=information.find_one({"name":f"{option}"})
+    if 'debt_to_equity' in k:
+        average_days=k['debt_to_equity']
+        if average_days=="NA":
+            return 0
+        else:
+            if not isinstance(average_days, str):
+                average_days=str(average_days)
+            average_days=average_days.replace("%","")
+            average_days=float(average_days)
+            return round(average_days,2)
+            # return round(average_days,2) #average_tenure
+    else:
+        return"average_tenure not found in any document"
+def Debt_to_Equity(option):
+        drreddy=debt('drreddy')
+        novartis=debt('novartis')
+        abbott=debt('abbott')
+        gsk=debt('gsk')
+        teva=debt('teva')
+        takeda=debt('takeda')
+        k={'drreddy': drreddy, 'novartis': novartis, 'abbott': abbott, 'gsk': gsk, 'teva': teva, 'takeda': takeda}
+        # print(k)
+        list1=sorted(k.values())
+        # print(list1)
+        op=k.get(option)
+        # print(op)
+        if op==0:
+            gsk_position=0
+        else:
+            gsk_position=list1.index(op)
+        # print(gsk_position)
+        percent=(gsk_position/len(list1))*100
+        # print(percent)
+        if percent==0:
+            gsk_percent1=0
+        else:
+            gsk_percent1=(100-percent)*0.03
+        return gsk_percent1
+
+
+# print(Debt_to_Equity('teva'))
--- views/reports.py
+++ views/reports.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 from RIsk_score.management_hsr import board_industry_skills,TSR,HSR
 # from pymongo import MongoClient
-
+from RIsk_score.Financial_score import Debt_to_Equity,EBIT_Net_Sales
 
 def load_reports():
     st.header("Reports Generator")
@@ -25,6 +25,13 @@ def load_reports():
         Hist=HSR(option)
         st.write(f"{option}'s Historical Share Price:",Hist)
         st.write(f"Total management score for {option}={tsr}+{board}+{Hist}:",tsr+board+Hist)
+
+        st.subheader('Financial Score')
+        ebit=EBIT_Net_Sales(option)
+        st.write(f"{option}'s EBIT/Net Sales:",ebit)
+        debt=Debt_to_Equity(option)
+        st.write(f"{option}'s Debt to Equity ratio:",debt)
+        st.write(f"Total financial score for {option}:",ebit+debt)
 
 
 
