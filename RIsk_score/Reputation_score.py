7c757d074da576cf59624fdf15b1cb395ef1f82d
jd-co1 <sheik.javeed@tynybay.com>
2024-02-19 20:11:52 +0530

    updated totla risk score

---
+++ RIsk_score/Reputation_score.py
@@ -0,0 +1,91 @@
+from pymongo import MongoClient
+import os,math,re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=MongoClient('mongodb+srv://test:test@cluster0.tw5ieeh.mongodb.net/?retryWrites=true&w=majority')
+mydb=client.get_database('Udbhata')
+information=mydb.companies_data
+
+
+def sustainability_db(option):
+    k=information.find_one({"name":f"{option}"})
+    if 'Sustainability' in k:
+        fin=k['Finance']
+        ESG=k['Sustainability']
+        Governance=k['Governance']
+        Risk=k['Risk_management']
+        if 'awards' in ESG:
+            awards=ESG['awards']
+            data_list=[]
+            for i in awards:
+                k=data_list.append(i.split('\n'))
+            # print(data_list)
+            if data_list[0][0]=='NA':
+                sus= 0
+            # print(data_list)
+            else:
+                sus= len([item for sublist in data_list for item in sublist])
+        if 'awards' in fin:
+            awards=fin['awards']
+            data_list=[]
+            for i in awards:
+                k=data_list.append(i.split('\n'))
+            # print(data_list)
+            if data_list[0][0]=='NA':
+                Fina= 0
+            # print(data_list)
+            else:
+                Fina= len([item for sublist in data_list for item in sublist])
+        if 'awards' in Governance:
+            awards=Governance['awards']
+            data_list=[]
+            for i in awards:
+                k=data_list.append(i.split('\n'))
+            # print(data_list)
+            if data_list[0][0]=='NA':
+                Gov= 0
+            # print(data_list)
+            else:
+                Gov= len([item for sublist in data_list for item in sublist])
+        if 'awards' in Risk:
+            awards=Risk['awards']
+            data_list=[]
+            for i in awards:
+                k=data_list.append(i.split('\n'))
+            # print(data_list)
+            if data_list[0][0]=='NA':
+                Rik= 0
+            else:
+                Rik= len([item for sublist in data_list for item in sublist])
+            # print(data_list)
+        return Fina+sus+Gov+Rik
+        
+        
+
+# print(sustainability_db("drreddy"))
+def Sustainability(option):
+    drreddy=sustainability_db('drreddy')
+    novartis=sustainability_db('novartis')
+    abbott=sustainability_db('abbott')
+    gsk=sustainability_db('gsk')
+    teva=sustainability_db('teva')
+    takeda=sustainability_db('takeda')
+    k={'drreddy': drreddy, 'novartis': novartis, 'abbott': abbott, 'gsk': gsk, 'teva': teva, 'takeda': takeda}
+    print(k)
+    list1=sorted(k.values())
+    print(list1)
+    op=k.get(option)
+    print(op)
+    gsk_position = len([x for x in list1 if x < op])
+    print(gsk_position)
+    percent=(gsk_position/len(list1))*100
+    print(percent)
+    if percent==0:
+        gsk_percent1=0
+    else:
+        gsk_percent1=(100-percent)*0.10
+    return gsk_percent1
+
+# print(Sustainability('teva'))
--- views/reports.py
+++ views/reports.py
@@ -6,6 +6,13 @@ from RIsk_score.management_hsr import board_industry_skills,TSR,HSR
 from RIsk_score.Financial_score import Debt_to_Equity,EBIT_Net_Sales
 from RIsk_score.company_specific import company_Count,Company_Impact
 from RIsk_score.Non_financial_score import Investment,ROCE,ESG
+from RIsk_score.Reputation_score import Sustainability
+from pymongo import MongoClient
+client=MongoClient('mongodb+srv://test:test@cluster0.tw5ieeh.mongodb.net/?retryWrites=true&w=majority')
+
+mydb=client.get_database('Udbhata')
+information=mydb.companies_data
+
 
 def load_reports():
     st.header("Reports Generator")
@@ -18,45 +25,66 @@ def load_reports():
         index=None,
         placeholder="companies")
     if option == 'drreddy' or option == 'gsk' or option == 'takeda' or option == 'teva' or option == 'abbott' or option == 'novartis':
-        st.write('You selected:', option)
-        st.subheader('Managemeant Score')
-        tsr=TSR(option)
-        st.write(f"{option}'s TSR:",tsr)
-        board=board_industry_skills(option)#board_industry_skills(option)
-        st.write(f"{option}'s Board Industry Experience:",board)
-        Hist=HSR(option)
-        st.write(f"{option}'s Historical Share Price:",Hist)
-        st.write(f"Total management score for {option}={tsr}+{board}+{Hist}:",round((tsr+board+Hist),2))
-
-        st.subheader('Financial Score')
-        ebit=EBIT_Net_Sales(option)
-        st.write(f"{option}'s EBIT/Net Sales:",ebit)
-        debt=Debt_to_Equity(option)
-        st.write(f"{option}'s Debt to Equity ratio:",debt)
-        st.write(f"Total financial score for {option}:",round((ebit+debt),2))
+        
+            st.write('You selected:', option)
+            st.subheader('Managemeant Score')
+            tsr=TSR(option)
+            st.write(f"{option}'s TSR:",tsr)
+            board=board_industry_skills(option)#board_industry_skills(option)
+            st.write(f"{option}'s Board Industry Experience:",board)
+            Hist=HSR(option)
+            st.write(f"{option}'s Historical Share Price:",Hist)
+            total1=tsr+board+Hist
+            st.write(f"Total management score for {option}:",round((total1),2))
 
 
-        st.subheader('Industry Risk Score')
-        st.write("NA")
 
-        st.subheader('Company Specific Risk Score')
-        impact=Company_Impact(option)
-        st.write(f"{option}'s Risk Impact:",impact)
-        count=company_Count(option)
-        st.write(f"{option}'s Risk Count:",count)
-        st.write(f"Total company specific score for {option}:",round((impact+count),2))
+            st.subheader('Financial Score')
+            ebit=EBIT_Net_Sales(option)
+            st.write(f"{option}'s EBIT/Net Sales:",ebit)
+            debt=Debt_to_Equity(option)
+            st.write(f"{option}'s Debt to Equity ratio:",debt)
+            total2=ebit+debt
+            st.write(f"Total financial score for {option}:",round((total2),2))
 
-        st.subheader("Country Specific Risk Score")
-        st.write("NA")
+            st.subheader('Industry Risk Score')
+            st.write("NA")
 
-        st.subheader("Non-financial Risk Score")
-        inves=Investment(option)
-        st.write(f"{option}'s current Investment:",inves)
-        roce=ROCE(option)
-        st.write(f"{option}'s ROCE:",roce)
-        esg=ESG(option)
-        st.write(f"{option}'s count of YES for ESG questions:",esg)
-        st.write(f"Total non-financial score for {option}:",round((inves+roce+esg),2))
+            st.subheader('Company Specific Risk Score')
+            impact=Company_Impact(option)
+            st.write(f"{option}'s Risk Impact:",impact)
+            count=company_Count(option)
+            st.write(f"{option}'s Risk Count:",count)
+            total3=impact+count
+            st.write(f"Total company specific score for {option}:",round((total3),2))
+
+            st.subheader("Country Specific Risk Score")
+            st.write("NA")
+
+            st.subheader("Non-financial Risk Score")
+            inves=Investment(option)
+            st.write(f"{option}'s current Investment:",inves)
+            roce=ROCE(option)
+            st.write(f"{option}'s ROCE:",roce)
+            esg=ESG(option)
+            st.write(f"{option}'s count of YES for ESG questions:",esg)
+            total4=inves+roce+esg
+            st.write(f"Total non-financial score for {option}:",round((total4),2))
+
+
+            st.subheader("Reputation Score")
+            award=Sustainability(option)
+            st.write(f"{option}'s 10% score of awards:",award)
+            total5=award
+            st.write(f"Total reputation score for {option}:",round((total5),2))
+
+
+            st.subheader("Overall Score")
+            total=total1+total2+total3+total4+total5
+            information.update_one({"name":f"{option}"},{"$set":{
+                                                    "total_risk_score":total
+                        }})
+            st.write(f"Total Overall score for {option}:",round((total),2))
 
 
 
